dc3b66e6c21f41816aec89d750d025b8682a0867
Vadzim Valchkovich <vol4k.w@gmail.com>
2025-05-31 13:22:03 +0200

    feat: Define NgRx Tasks Actions

---
+++ src/app/state/tasks/task.actions.ts
@@ -0,0 +1,59 @@
+import { createAction, props } from '@ngrx/store';
+import { Task } from '../../models/task.model';
+
+export const loadTasks = createAction('[Task Page] Load Tasks');
+
+export const loadTasksSuccess = createAction(
+  '[Task API] Load Tasks Success',
+  props<{ tasks: Task[] }>()
+);
+
+export const loadTasksFailure = createAction(
+  '[Task API] Load Tasks Failure',
+  props<{ error: string }>()
+);
+
+export const addTask = createAction(
+  '[Task Form] Add task',
+  props<{ task: Task }>()
+);
+
+export const addTaskSuccess = createAction(
+  '[Task API] Add Task Success',
+  props<{ task: Task }>()
+);
+
+export const addTaskFailure = createAction(
+  '[Task API] Add Task Failure',
+  props<{ error: string }>()
+);
+
+export const toggleTaskComplete = createAction(
+  '[Task Item] Toggle Task Complete',
+  props<{ taskId: string }>()
+);
+
+export const toggleTaskCompleteSuccess = createAction(
+  '[Task API] Toggle Task Complete Success',
+  props<{ task: Task }>()
+);
+
+export const toggleTaskCompleteFailure = createAction(
+  '[Task API] Toggle Task Complete Failure',
+  props<{ error: string }>()
+);
+
+export const deleteTask = createAction(
+  '[Task Item] Delete Task',
+  props<{ taskId: string }>()
+);
+
+export const deleteTaskSuccess = createAction(
+  '[Task API] Delete Task Success',
+  props<{ taskId: string }>()
+);
+
+export const deleteTaskFailure = createAction(
+  '[Task API] Delete Task Failure',
+  props<{ error: string }>()
+);
