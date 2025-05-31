2ee2db6f5da23f96e15e9ff49de985f1cb41c1b5
Vadzim Valchkovich <vol4k.w@gmail.com>
2025-05-31 15:07:54 +0200

    feat: Register tasks reducer in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,11 +9,15 @@ import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 
+import { tasksReducer } from './state/tasks/tasks.reducer';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(),
-    provideStore({}),
+    provideStore({
+      tasks: tasksReducer,
+    }),
     provideEffects([]),
     provideStoreDevtools({
       maxAge: 25,
