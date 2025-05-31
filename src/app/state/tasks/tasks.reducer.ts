777c68b786b9a27579b3b6e7996b2c6fa5f06008
Vadzim Valchkovich <vol4k.w@gmail.com>
2025-05-31 15:04:31 +0200

    feat: Implement NgRx Task Reducer with entity adapter

---
+++ src/app/state/tasks/tasks.reducer.ts
@@ -0,0 +1,26 @@
+import { createReducer, on } from '@ngrx/store';
+import { initialTasksState, tasksAdapter } from './tasks.state';
+import * as TaskActions from './task.actions';
+
+export const tasksReducer = createReducer(
+  initialTasksState,
+
+  on(TaskActions.loadTasksSuccess, (state, { tasks }) => {
+    return tasksAdapter.setAll(tasks, state);
+  }),
+
+  on(TaskActions.addTaskSuccess, (state, { task }) => {
+    return tasksAdapter.addOne(task, state);
+  }),
+
+  on(TaskActions.toggleTaskCompleteSuccess, (state, { task }) => {
+    return tasksAdapter.updateOne(
+      { id: task.id, changes: { completed: task.completed } },
+      state
+    );
+  }),
+
+  on(TaskActions.deleteTaskSuccess, (state, { taskId }) => {
+    return tasksAdapter.removeOne(taskId, state);
+  })
+);
