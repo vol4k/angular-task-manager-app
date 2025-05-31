9266a370f9914d2023e1d36d1760e430d7e0e8c6
Vadzim Valchkovich <vol4k.w@gmail.com>
2025-05-31 18:04:54 +0200

    feat: Update TaskListComponent to use NgRx selectors and dispatch load action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { Task } from './models/task.model';
 import { TaskListComponent } from './task-list/task-list.component';
 
 @Component({
@@ -12,10 +11,4 @@ import { TaskListComponent } from './task-list/task-list.component';
 })
 export class AppComponent {
   title = 'angular-task-manager-app';
-
-  dummyTasks: Task[] = [
-    { id: '1', title: 'Learn Angular basics', completed: false },
-    { id: '2', title: 'Setup NgRx', completed: true },
-    { id: '3', title: 'Style with SCSS and Flexbox', completed: false },
-  ];
 }
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,7 +1,11 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Task } from '../models/task.model';
 import { TaskItemComponent } from '../task-item/task-item.component';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import * as TaskSelectors from '../state/tasks/task.selectors';
+import * as TaskActions from '../state/tasks/task.actions';
 
 @Component({
   selector: 'app-task-list',
@@ -11,15 +15,23 @@ import { TaskItemComponent } from '../task-item/task-item.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TaskListComponent {
-  @Input() tasks: Task[] | null = [];
+  tasks$: Observable<Task[]>;
 
-  // TODO onToggleTaskComplete implementation
-  onToggleTaskComplete(taskId: string): void {
-    console.log('Toggle complete for task ID:', taskId);
+  constructor(private store: Store) {
+    this.tasks$ = this.store.select(TaskSelectors.selectAll);
   }
 
-  // TODO onDeleteTask implementation
+  ngOnInit(): void {
+    this.store.dispatch(TaskActions.loadTasks());
+  }
+
+  onToggleTaskComplete(taskId: string): void {
+    console.log('Toggle complete for task ID:', taskId);
+    this.store.dispatch(TaskActions.toggleTaskComplete({ taskId }));
+  }
+
   onDeleteTask(taskId: string): void {
     console.log('Delete task ID:', taskId);
+    this.store.dispatch(TaskActions.deleteTask({ taskId }));
   }
 }
